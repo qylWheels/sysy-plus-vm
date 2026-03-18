5a2cb2dd7f79c17c6afb54eac297bcde5bd27157
qylWheels <command1748165360@126.com>
2026-03-18 20:27:05 +0800

    实现main函数的逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <format>
 #include <iostream>
 
+#include "parse.h"
 #include "vm.h"
 
 int main(int argc, char *argv[]) {
-  auto path{argv[0]};
+  auto parser = Parser();
+  auto prog = parser.parse(argv[1]);
+  auto vm = Vm();
+  vm.load_program(prog);
+  vm.run();
 
   return 0;
 }
