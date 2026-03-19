f27fd3935d6fe76e3fadefa9b7e841b56aea5bdd
qylWheels <command1748165360@126.com>
2026-03-19 22:55:26 +0800

    增加字节码文件常量表中数字类型的定义

--- src/value.h
+++ src/value.h
@@ -3,8 +3,13 @@
 #include <cstdint>
 #include <variant>
 
+// 字节码文件中值的类型的定义
+enum class ValueType : std::uint8_t { Number };
+
+// 格式：[tag:8][value:64]
 using NumberValue = double;
 
+// 格式：字节码文件中的常量池里不会有这个玩意。只有在运行时才有它
 struct FunctionValue {
   std::uint16_t index;  // 函数在函数表中的下标
 };
