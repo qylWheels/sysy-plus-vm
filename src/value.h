ff430e7af318ab4d6864bdb403204f25e0a25142
qylWheels <command1748165360@126.com>
2026-03-20 22:53:39 +0800

    新增boolean value类型

--- src/value.h
+++ src/value.h
@@ -4,9 +4,12 @@
 #include <variant>
 
 // 字节码文件中值的类型的定义
-enum class ValueType : std::uint8_t { Number };
+enum class ValueType : std::uint8_t { Boolean, Number };
 
-// 格式：[tag:8][value:64]
+// 格式: [tag:8][reserved:63][value:1]
+using BooleanValue = bool;
+
+// 格式: [tag:8][value:64]
 using NumberValue = double;
 
 // 格式：字节码文件中的常量池里不会有这个玩意。只有在运行时才有它
