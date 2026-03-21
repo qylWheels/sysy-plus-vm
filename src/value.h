b4aa6055dc9675427bb5eabe204ec5ff3ec7186a
qylWheels <command1748165360@126.com>
2026-03-21 22:40:36 +0800

    修改了一些值的格式

--- src/value.h
+++ src/value.h
@@ -3,18 +3,18 @@
 #include <cstdint>
 #include <variant>
 
-// 字节码文件中值的类型的定义
-enum class ValueType : std::uint8_t { Boolean, Number };
+// 字节码文件常量池中值的类型的定义
+enum class ValueType : std::uint8_t { Boolean, Number, Function };
 
-// 格式: [tag:8][reserved:63][value:1]
+// 格式: [tag:8][reserved:7][value:1]
 using BooleanValue = bool;
 
 // 格式: [tag:8][value:64]
 using NumberValue = double;
 
-// 格式：字节码文件中的常量池里不会有这个玩意。只有在运行时才有它
+// 格式：[tag:8][index:16]
 struct FunctionValue {
-  std::uint16_t index;  // 函数在函数表中的下标
+  std::uint16_t index;  // 函数在函数表（不论是文件中的还是vm中的）中的下标
 };
 
 using Value = std::variant<BooleanValue, NumberValue, FunctionValue>;
