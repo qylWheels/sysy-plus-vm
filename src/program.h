def7df845cbd5bc931278a2f5e17fcbfa4aba0d2
qylWheels <command1748165360@126.com>
2026-03-21 21:44:37 +0800

    修改字节码文件结构和Program结构体字段

--- src/program.h
+++ src/program.h
@@ -10,17 +10,15 @@
 // [4字节] magic number: 0x53 0x59 0x73 0x79 ("SYsy")
 // [2字节] 次版本号
 // [2字节] 主版本号
-// [4字节] 常量池大小（条目数量）
-// [sizeof(constant_value，定长) * N] 常量池
 // [4字节] 函数个数
+// [4字节 * N] 每个函数的函数名长度
+// [4字节 * N] 每个函数的指令条数
 // [sizeof(function，变长) * N] 函数
+// - [变长] 函数名
+// - [变长] 函数指令
 
 struct Program {
-  std::uint32_t magic_number;
   std::uint16_t minor_version;
   std::uint16_t major_version;
-  std::uint32_t constant_count;
-  std::vector<Value> constants;
-  std::uint32_t func_count;
   std::vector<Function> funcs;
 };
