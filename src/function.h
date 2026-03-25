daeb7e6190230e57e51058ab78da36d9a3634ffa
qylWheels <command1748165360@126.com>
2026-03-25 14:21:29 +0800

    更改Function结构体的定义

--- src/function.h
+++ src/function.h
@@ -4,9 +4,11 @@
 #include <string>
 #include <vector>
 
-// 格式：[function name length:16][function name:var][instruction
-// count:32][instructions:var]
+// 格式：
+// [function name length:8][function name:var][param count:8]
+// [instruction count:32][instructions:var]
 struct Function {
   std::string name;
+  std::uint8_t param_count;
   std::vector<std::uint32_t> instructions;
 };
