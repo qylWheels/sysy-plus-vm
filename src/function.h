4f8fd513a6bd5e0b5c166fb492bd06990680c8fe
qylWheels <command1748165360@126.com>
2026-03-30 21:43:07 +0800

    更改Function的定义

--- src/function.h
+++ src/function.h
@@ -5,10 +5,13 @@
 #include <vector>
 
 // 格式：
-// [function name length:8][function name:var][param count:8]
+// [function name length:8][function name:var]
+// [param count:8]
+// [local variable count:16]
 // [instruction count:32][instructions:var]
 struct Function {
   std::string name;
   std::uint8_t param_count;
+  std::uint16_t local_var_count;
   std::vector<std::uint32_t> instructions;
 };
